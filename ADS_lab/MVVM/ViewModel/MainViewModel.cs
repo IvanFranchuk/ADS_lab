91d313a2d38c5a55c2b9a23b68352185ab9a16ff
Ivan Franchuk <franchukivan123@gmail.com>
2023-09-11 18:13:40 +0300

    ui

---
+++ ADS_lab/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using ADS_lab.Core;
+using System;
+
+
+namespace ADS_lab.MVVM.ViewModel
+{
+    class MainViewModel : ObservalbleObject
+    {
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
